7a2cb0a456de5c1c2e9be226285aee2c0cb9b2ba
Chris Hunter <orkem2@gmail.com>
2017-08-04 13:12:30 +0100

    changed parameters in guitar constructor

--- music_management/Guitar.java
+++ music_management/Guitar.java
@@ -4,10 +4,12 @@ public class Guitar extends Instrument{
 
   int numberStrings;
 
-  public Guitar(int numberStrings){
+  public Guitar(InstrumentColour colour, InstrumentType type, int numberStrings){
     super(colour, type);
     this.numberStrings = numberStrings;
   }
 
-
+  public String play(){
+    return "Strum";
+  }
 }
