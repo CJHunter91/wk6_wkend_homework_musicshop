5e8c78621136b61d4f96c511e5082dcc2e96b228
Chris Hunter <orkem2@gmail.com>
2017-08-04 14:56:25 +0100

    added test to shoptest to check addstock method in shop

--- music_management_spec/ShopTest.java
+++ music_management_spec/ShopTest.java
@@ -6,14 +6,23 @@ import music_management.*;
 public class ShopTest{
 
   Shop shop;
+  Sellable guitar;
 
   @Before
   public void before(){
     shop = new Shop();
+    guitar = new Guitar(6, InstrumentType.STRINGS, InstrumentColour.RED, 100, 200); 
+
   }
 
   @Test
   public void canCountStock(){
     assertEquals(0, shop.countStock());
+  }
+
+  @Test
+  public void canAddStock(){
+
+    assertEquals(1, shop.addStock(guitar));
   }
 }
