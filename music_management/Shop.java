f8a855d5f6ad91132080486b445e9ddf7472b4d1
Chris Hunter <orkem2@gmail.com>
2017-08-04 14:46:33 +0100

    created shop class

---
+++ music_management/Shop.java
@@ -0,0 +1,11 @@
+package music_management;
+import java.util.*;
+import behaviours.*;
+
+public class Shop{
+  private ArrayList<Sellable> stock;
+
+  public Shop(){
+    this.stock = new ArrayList<Sellable>();
+  }
+}
